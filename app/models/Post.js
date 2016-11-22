c2d3beb4ff6cc5168c25906e5f8e2ac7a2cb3481
wuyanxin <yanxin.wu@qq.com>
2016-11-22 22:26:19 +0800

    ✨ add field on Post

--- app/models/Post.js
+++ app/models/Post.js
@@ -4,12 +4,16 @@ let Post = {
   name: 'post',
   attributes: {
     title: Sequelize.STRING,
-    description: Sequelize.TEXT,
+    slug: Sequelize.STRING,
+    description: Sequelize.STRING(500),
     md: Sequelize.TEXT,
     content: Sequelize.TEXT,
-    image: Sequelize.STRING,
+    images: Sequelize.STRING(1000),
+
     type: Sequelize.ENUM('post', 'page'),
     category: Sequelize.STRING, // 文章在细分技术类、生活类、读书笔记等大类
+    featured: Sequelize.BOOLEAN, // true 置顶
+
     // tags
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   },
--- config.js
+++ config.js
@@ -9,8 +9,8 @@ const config = {
       name: '吴彦欣',
       avatar: 'https://www.gravatar.com/avatar/0531873f81c71a5a9ee9e3071a4ffa87?s=250&d=mm&r=x',
       sns: [
-        { logo: 'socicon-github', url: 'http://github.com/wuyanxin', title: 'GitHub' },
         { logo: 'socicon-stackoverflow', url: 'http://stackoverflow.com/users/5550807/ethan-wu', title: 'StackoverFlow' },
+        { logo: 'socicon-github', url: 'http://github.com/wuyanxin', title: 'GitHub' },
         { logo: 'socicon-rss', url: 'http://wuyanxn.com/fead', title: 'Feed' },
       ]
     },
