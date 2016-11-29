87645c8a9845dc0a72e3affd4e23e7d2d0d45fd4
wuyanxin <yanxin.wu@qq.com>
2016-11-29 23:35:05 +0800

    ✨ cors support for development

--- app.js
+++ app.js
@@ -36,6 +36,14 @@ console.log('after')
 
 // load middlewares TODO
 
+if (app.env === 'development') {
+  app.use(function* (next) {
+    this.set('Access-Control-Allow-Credentials', true);
+    this.set('Access-Control-Allow-Origin', '*');
+    yield next;
+  });
+}
+
 // x-response-time
 app.use(function* (next){
   var start = new Date;
